5e6e6aeb6e39a1c227b6a7428dce80794ac0d2bd
mikolajsmigielski <mikolaj.smigielski@gmail.com>
2021-07-08 19:04:36 +0200

    Create tile prefab and script for tile rotation

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public bool Active = true;
+    void Start()
+    {
+
+        transform.rotation = GetTargetRotation();
+    }
+
+    
+    void Update()
+    {
+        var TargetRotation = GetTargetRotation();
+        transform.rotation = Quaternion.Lerp(transform.rotation, TargetRotation, Time.deltaTime * 5f);
+    }
+    Quaternion GetTargetRotation()
+    {
+        var rotation = Active ? Vector3.zero : (Vector3.up * 180f);
+        return Quaternion.Euler(rotation);
+    }
+}
